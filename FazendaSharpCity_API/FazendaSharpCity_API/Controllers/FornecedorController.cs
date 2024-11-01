7c2ac5d70040efc62085d9b0f14a8bd4d3774ddc
ViniciusDeolino <vinicius.deolino20@gmail.com>
2024-11-01 08:48:21 -0300

    Criando atributos fornecedor e crinado a controller

--- FazendaSharpCity_API/FazendaSharpCity_API/Controllers/FornecedorController.cs
+++ FazendaSharpCity_API/FazendaSharpCity_API/Controllers/FornecedorController.cs
@@ -1,12 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FazendaSharpCity_API.Data;
+using FazendaSharpCity_API.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FazendaSharpCity_API.Controllers
 {
     public class FornecedorController : Controller
     {
-        public IActionResult Index()
+        private FornecedorContext _context;
+
+        public FornecedorController(FornecedorContext context)
         {
-            return View();
+            _context = context;
         }
-    }
+
+        [HttpPost]
+        public async Task<IActionResult> AdicionarFornecedor([FromBody] Fornecedor fornecedor)
+        {
+            if(fornecedor == null)
+            {
+                return BadRequest("Fornecedor não pode ser nulo.");
+            }
+
+            await _context.Fornecedores.AddAsync(fornecedor);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(ConsultaFornecedor), new { id = fornecedor.Id }, fornecedor.Id);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ConsultaFornecedor(int id)
+        {
+            var fornecedor = await _context.Fornecedores.FindAsync(id);
+
+            if(fornecedor == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(fornecedor);
+            }
+        }
+    }       
 }
--- FazendaSharpCity_API/FazendaSharpCity_API/Models/Fornecedor.cs
+++ FazendaSharpCity_API/FazendaSharpCity_API/Models/Fornecedor.cs
@@ -2,6 +2,17 @@
 {
     public class Fornecedor
     {
-
+        public int Id { get; set; }
+        public string RazaoSocial { get; set; }
+        public string NomeFantasia { get; set; }
+        public string Cnpj { get; set; }
+        public string Email { get; set; }
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+        public string Bairro { get; set; }
+        public int Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Cep { get; set; }
+        public string TelefoneFornecedor { get; set; }
     }
 }
