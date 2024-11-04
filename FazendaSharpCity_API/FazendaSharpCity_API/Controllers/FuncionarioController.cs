e9de54261f3a66249d4704a553ea95c08a585d62
ViniciusDeolino <vinicius.deolino20@gmail.com>
2024-11-04 11:21:52 -0300

    Implementacao dos atributos e metodos na classe funcionario e controller

--- FazendaSharpCity_API/FazendaSharpCity_API/Controllers/FuncionarioController.cs
+++ FazendaSharpCity_API/FazendaSharpCity_API/Controllers/FuncionarioController.cs
@@ -1,12 +1,73 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using FazendaSharpCity_API.Data.Contexts;
+using FazendaSharpCity_API.Data.DTOs.Cliente;
+using FazendaSharpCity_API.Data.DTOs.Funcionario;
+using FazendaSharpCity_API.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FazendaSharpCity_API.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
     public class FuncionarioController : Controller
     {
-        public IActionResult Index()
+        private FuncionarioContext _context;
+        private IMapper _mapper;
+
+        public FuncionarioController(FuncionarioContext context, IMapper mapper)
         {
-            return View();
+            _context = context;
+            _mapper = mapper;
         }
+
+        [HttpPost]
+
+        public async Task<IActionResult> CreateFuncionario([FromBody] CreateFuncionarioDto funcionarioDto)
+        {
+            Funcionario funcionario = _mapper.Map<Funcionario>(funcionarioDto);
+
+            if(funcionario == null)
+            {
+                return BadRequest("Funcionadrio não pode ser nulo");
+            }
+
+            await _context.Funcionarios.AddAsync(funcionario);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetFuncionario), new { id = funcionario.Id }, funcionario.Id);
+        }
+
+        [HttpGet("{id}")]
+
+        public async Task<IActionResult>GetFuncionario(int id)
+        {
+            var funcionario = await _context.Funcionarios.FindAsync(id);
+
+            if(funcionario == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(funcionario);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult>UpdateFuncionario(int id, [FromBody] UpdateFuncionarioDto funcionarioDto)
+        {
+            var funcionario = _context.Funcionarios.FirstOrDefault(fornecedor => fornecedor.Id == id);
+
+            if (funcionario == null)
+            {
+                return NotFound();
+            }
+
+            _mapper.Map(funcionarioDto, funcionario);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
     }
 }
--- FazendaSharpCity_API/FazendaSharpCity_API/Models/Funcionario.cs
+++ FazendaSharpCity_API/FazendaSharpCity_API/Models/Funcionario.cs
@@ -2,6 +2,21 @@
 {
     public class Funcionario
     {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Email { get; set; }
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+        public string Bairro{ get; set; }
+        public int Numero{ get; set; }
+        public string Complemento { get; set; }
+        public string Cep { get; set; }
+        public decimal Salario { get; set; }
+        public string Login { get; set; }
+        public string Senha { get; set; }
+        public string TelefoneFuncionario { get; set; }
 
     }
 }
