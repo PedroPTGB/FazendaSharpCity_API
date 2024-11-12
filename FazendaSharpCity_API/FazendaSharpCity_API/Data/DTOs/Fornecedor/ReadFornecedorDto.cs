09697e44bf59210b57d2e12ff947469a9e4218fc
Vinicius Vinicius <vinicius.deolino20@gmail.com>
2024-11-11 21:58:37 -0300

    Atualização dados fornecedor

--- FazendaSharpCity_API/FazendaSharpCity_API/Data/DTOs/Fornecedor/ReadFornecedorDto.cs
+++ FazendaSharpCity_API/FazendaSharpCity_API/Data/DTOs/Fornecedor/ReadFornecedorDto.cs
@@ -1,6 +1,19 @@
-﻿namespace FazendaSharpCity_API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FazendaSharpCity_API.Models
 {
     public class ReadFornecedorDto
     {
+        public string RazaoSocial { get; set; }
+        public string NomeFantasia { get; set; }        
+        public string Cnpj { get; set; }        
+        public string Email { get; set; }
+        public string Estado { get; set; }        
+        public string Cidade { get; set; }       
+        public string Bairro { get; set; }        
+        public int Numero { get; set; }       
+        public string Complemento { get; set; }        
+        public string Cep { get; set; }
+        public string TelefoneFornecedor { get; set; }
     }
 }
