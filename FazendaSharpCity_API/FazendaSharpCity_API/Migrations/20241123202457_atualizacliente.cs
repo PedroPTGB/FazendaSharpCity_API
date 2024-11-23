5d1e5e1e8cdc2dcf755475a396a003f85388cf6d
Pedro <pedro.paulo.tgb@gmail.com>
2024-11-23 19:44:53 -0300

    atualização retirando docker

---
+++ FazendaSharpCity_API/FazendaSharpCity_API/Migrations/20241123202457_atualizacliente.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FazendaSharpCity_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class atualizacliente : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Sexo",
+                table: "Clientes");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<char>(
+                name: "Sexo",
+                table: "Clientes",
+                type: "character(1)",
+                nullable: false,
+                defaultValue: '\0');
+        }
+    }
+}
