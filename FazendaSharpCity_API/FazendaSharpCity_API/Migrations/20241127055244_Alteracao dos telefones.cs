da81b468996f1f347e2bd7045c74d69b62e5f54c
Pedro <pedro.paulo.tgb@gmail.com>
2024-11-27 03:12:13 -0300

    alteração do gitignore

---
+++ FazendaSharpCity_API/FazendaSharpCity_API/Migrations/20241127055244_Alteracao dos telefones.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FazendaSharpCity_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Alteracaodostelefones : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TelefoneFuncionario",
+                table: "Funcionarios",
+                newName: "Telefone");
+
+            migrationBuilder.RenameColumn(
+                name: "TelefoneFornecedor",
+                table: "Fornecedores",
+                newName: "Telefone");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Telefone",
+                table: "Funcionarios",
+                newName: "TelefoneFuncionario");
+
+            migrationBuilder.RenameColumn(
+                name: "Telefone",
+                table: "Fornecedores",
+                newName: "TelefoneFornecedor");
+        }
+    }
+}
